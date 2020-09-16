45ca58fdd1495cdda24c7cc404abe4b82c8a5838
lopsangc2 <lopsangc2@gmail.com>
2020-09-16 13:00:04 +0545

    finished parsing data and storing the data to csv file not done

--- scrapy.py
+++ scrapy.py
@@ -1,14 +1,37 @@
 from bs4 import BeautifulSoup
 import requests
+import csv
 
-with open('simple.html') as html_files:
-	soup = BeautifulSoup(html_files, 'lxml')
 
-# print(soup.prettify())
+source = requests.get('http://coreyms.com').text
+ 
+soup = BeautifulSoup(source, 'lxml')
 
-for match in soup.find_all('div', class_='article'):
-	headline = match.h2.a.text
-	print(headline)
 
-	paragraph = match.p.text
-	print(paragraph)
+
+
+
+for article in soup.find_all('article'):
+
+	title = article.header.h2.a.text
+	print(title)
+
+	summary = article.find('div', class_='entry-content').p.text
+	print(summary)
+
+	try:
+		vid_src = article.find('iframe', class_='youtube-player')['src']
+		vid_id = vid_src.split('/')[4]
+		vid_id = vid_id.split('?')[0]
+
+		yt_link = f'https://youtube.com/watch?v={vid_id}'
+		print(yt_link)
+
+		
+	except:
+		print(">>> --- !!! Link not found !!! --- <<<")
+
+	print()
+
+
+
