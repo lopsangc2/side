178cf6494d87d608b62d046bb161d63fcc45ba95
lopsangc2 <lopsangc2@gmail.com>
2020-09-19 18:01:07 +0545

    to github

--- himalayan.py
+++ himalayan.py
@@ -2,29 +2,35 @@ from bs4 import BeautifulSoup
 import requests
 import csv
 
+ul = 'https://kathmandupost.ekantipur.com'
 
-source = requests.get('https://kathmandupost.ekantipur.com').text
+
+source = requests.get(ul).text
 soup = BeautifulSoup(source, 'lxml')
-with open('KathmanduPostNews.csv', 'w') as csv_file:
-	csv_writer = csv.writer(csv_file)
-	csv_writer.writerow(['title', 'summary', 'article-link'] )
 
-	#title = soup.find('div', class_='row order')
-	head = soup.find('div', class_='grid-first')
+def kathmadnupost():
 
-	for article in head.find_all('article', class_='article-image'):
+	with open('KathmanduPostNews.csv', 'w') as csv_file:
+		csv_writer = csv.writer(csv_file)
+		csv_writer.writerow(['title', 'summary', 'article-link'] )
 
-		title = article.h3.a.text
-		print(title)
-		summary = article.p.text
-		print(summary)
-		try:
-			image_link = article.find('a')['href']
-			print(image_link)
-		except:
-			print("link not found")
-		print()
+		#title = soup.find('div', class_='row order')
+		head = soup.find('div', class_='grid-first')
 
-		csv_writer.writerow([title, summary, image_link])
+		for article in head.find_all('article', class_='article-image'):
+
+			title = article.h3.a.text
+			print(title)
+			summary = article.p.text
+			print(summary)
+			try:
+				image_link = article.find('a')['href']
+				print(image_link)
+			except:
+				print("link not found")
+			print()
+
+			csv_writer.writerow([title, summary, image_link])
 
 
+kathmadnupost()
